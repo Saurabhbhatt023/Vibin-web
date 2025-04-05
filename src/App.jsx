8f153ca638bb88ffe05c0eb9800c3afc99f4b5ab
Saurabh <saurabhbhatt023@gmail.com>
2025-04-05 21:47:37 +0530

    fixed the bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import { useSelector } from "react-redux";
-import { Navigate, useLocation } from "react-router-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import appStore from "./utils/appStore";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Feed from "./components/Feed";
+import Profile from "./components/Profile";
+import Connections from "./components/Connections";
+import Request from "./components/Request";
+import Chat from "./components/Chat";
+import PrivacyPolicy from "./components/PrivacyPolicy";
+import TermsOfService from "./components/TermsOfService";
+import RefundPolicy from "./components/RefundPolicy";
 
-const AuthProtected = ({ children }) => {
-  const user = useSelector((store) => store.user);
-  const location = useLocation();
-  
-  if (!user) {
-    // Redirect to login and store the attempted URL to redirect back after login
-    return <Navigate to="/login" state={{ from: location.pathname }} />;
-  }
-  
-  return children;
+const App = () => {
+  return (
+    <Provider store={appStore}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route index element={<Feed />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/connections" element={<Connections />} />
+            <Route path="/requests" element={<Request />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
+            <Route path="/refund-policy" element={<RefundPolicy />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 };
 
-export default AuthProtected;
+export default App;
